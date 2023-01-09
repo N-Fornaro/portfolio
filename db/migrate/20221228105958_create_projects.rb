ba5378c27cba8d37c668cde6938da23d2e8caece
Nicolas Fornaro <nicolasfornaro75@gmail.com>
2023-01-09 14:53:08 +0100

    initialization-models-controllers-viewHome

---
+++ db/migrate/20221228105958_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.date :start_date
+      t.date :end_date
+      t.string :team, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
